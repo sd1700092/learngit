d10eafe9c201051cb803accb65c6f0edd79cba4e
sd1700092 <foreverskyone@hotmail.com>
2018-03-05 22:26:10 +0800

    finished CH4-3

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -6,6 +6,21 @@ import (
 	"imooc.com/learngo/tree"
 )
 
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
 func main() {
 	var root tree.Node
 	//fmt.Println(root)
@@ -37,4 +52,9 @@ func main() {
 	root.Left.Right = tree.CreateNode(2)
 	root.Right.Left.SetValue(4)
 	root.Traverse()
+
+	fmt.Println()
+	myRoot := myTreeNode{&root}
+	myRoot.postOrder()
+	fmt.Println()
 }
