8fa29fa46de1244c0bd3a285384815d79859836d
sd1700092 <foreverskyone@hotmail.com>
2018-03-13 07:39:29 +0800

    finish 8-1

--- container/nonrepeating_test.go
+++ container/nonrepeating_test.go
@@ -29,3 +29,20 @@ func TestSubstr(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkSubstr(b *testing.B) {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s += s
+	}
+	ans := 8
+
+	b.Logf("len(s) = %d", len(s))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ans {
+			b.Errorf("got %d for input %s; expected %d", actual, s, ans)
+		}
+	}
+}
