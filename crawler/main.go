aada7ba8371f4b54301fb214d52d84f8a6f9d73b
沈天一 <tianyi3@eastmoney.com>
2018-06-06 09:28:28 +0800

    finish 15-1

--- crawler/main.go
+++ crawler/main.go
@@ -15,61 +15,8 @@ import (
 )
 
 func main() {
-	/*resp, err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	//if resp.StatusCode == http.StatusOK {
-	//	all, err := ioutil.ReadAll(resp.Body)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	fmt.Printf("%s\n", all)
-	//}
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code", resp.StatusCode)
-		return
-	}
-
-	utf8Reader := transform.NewReader(resp.Body, determinEncoding(resp.Body).NewDecoder())
-
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Printf("%s\n", all)*/
-	//printCityList(all)
 	engine.Run(engine.Request{
-		Url: "http://www.zhenai.com/zhenghun",
+		Url:        "http://www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
 }
-
-/*
-func determinEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-}
-
-func printCityList(contents []byte) {
-	//re := regexp.MustCompile(`<a href="http://www.zhenai.com/zhenghun/\w+"[^>]*>[^<]+</a>`)
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/\w+)"[^>]*>([^<]+)</a>`)
-	//matches := re.FindAll(contents, -1)
-	matches := re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		//fmt.Printf("%s\n", m)
-		//for _, subMatch := range m {
-			//fmt.Printf("%s ", subMatch)
-			fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
-		//}
-		fmt.Println()
-	}
-	fmt.Printf("Matches found: %d\n", len(matches))
-}*/
---
+++ rpc/rpc.go
@@ -0,0 +1,9 @@
+package rpcdemo
+
+// 我们的RPC最终是怎么被客户所调用的呢？Service.Method的方法
+type DemoService struct{}
+
+//Go语言语法框架对于RPC的要求就是要有args和result两个参数
+func (DemoService) Div(args, result) error {
+
+}
