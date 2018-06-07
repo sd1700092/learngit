be9c364a5b8f971213e8082de975ef81b3ab8184
sd1700092 <foreverskyone@hotmail.com>
2018-06-07 14:03:46 +0800

    finish 15-1

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -1,6 +1,8 @@
 package engine
 
-import "fmt"
+import (
+	"log"
+)
 
 type ConcurrentEngine struct {
 	Scheduler   Scheduler
@@ -9,24 +11,41 @@ type ConcurrentEngine struct {
 
 type Scheduler interface {
 	Submit(Request)
+	ConfigureMasterWorkerChan(chan Request)
 }
 
-func (e ConcurrentEngine) Run(seeds ...Request) {
-	for _, r := range seeds {
-		e.Scheduler.Submit(r)
-	}
+func (e *ConcurrentEngine) Run(seeds ...Request) {
 	in := make(chan Request)
 	out := make(chan ParseResult)
+	e.Scheduler.ConfigureMasterWorkerChan(in)
 	for i := 0; i < e.WorkerCount; i++ {
 		createWorker(in, out)
 	}
+
+	for _, r := range seeds {
+		e.Scheduler.Submit(r)
+	}
+
 	for {
 		result := <-out
 		for _, item := range result.Items {
-			fmt.Printf("Got item: %v\n", item)
+			log.Printf("Got item: %v\n", item)
 		}
 		for _, request := range result.Requests {
 			e.Scheduler.Submit(request)
 		}
 	}
+}
+
+func createWorker(in chan Request, out chan ParseResult) {
+	go func() {
+		for {
+			request := <-in
+			result, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- result
+		}
+	}()
 }
