7946969fd0c25b68d914f40c9a9bcf683f56aa38
sd1700092 <foreverskyone@hotmail.com>
2018-06-09 21:36:59 +0800

    finish 15-6

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -7,12 +7,14 @@ import (
 )
 
 //<a href="http://album.zhenai.com/u/108415017" target="_blank">惠儿</a>
-const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+var (
+	profileRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityUrlRe = regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[^"]+)"`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult {
 	//fmt.Printf("%s\n", contents)
-	re := regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		name := string(m[2])
@@ -20,6 +22,15 @@ func ParseCity(contents []byte) engine.ParseResult {
 		result.Requests = append(result.Requests, engine.Request{Url: string(m[1]), ParserFunc: func(c []byte) engine.ParseResult {
 			return ParseProfile(c, name)
 		}})
+	}
+
+	matches = cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m:=range matches{
+		result.Requests = append(result.Requests,
+			engine.Request{
+				Url: string(m[1]),
+				ParserFunc: ParseCity,
+			})
 	}
 	return result
 }
